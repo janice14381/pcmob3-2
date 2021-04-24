abda5147394d70e08ab22da1d13896da85e030a8
janice14381 <janice_czh@yahoo.com.sg>
2021-04-24 12:24:47 +0800

    add note in navi

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState, useEffect } from "react";
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import * as SQLite from 'expo-sqlite';
+import { SimpleLineIcons } from '@expo/vector-icons';
+
+
+
+const Stack = createNativeStackNavigator();
+const db = SQLite.openDatabase('notes.db');
+
+function addNote(){
+ console.log('add notes');
+}
+
+function NoteScreen({navigation}){
+  useEffect(
+    () =>{
+      navigation.setOptions({
+        headerRight: () =>(
+          <TouchableOpacity onPress={addNote}>
+            <SimpleLineIcons
+              name="note"
+              size={20}
+              color="black"
+              style={{marginRight:20}}
+            />  
+          </TouchableOpacity>
+        )
+      })
+    }
+  );
+
+
+  return(
+    <View style={styles.container}>
+      <Text>note</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Notes" 
+        component={NoteScreen} 
+        options={{
+          headerTitle: "Todo Apps",
+          headerTitleStyle:{
+            fontWeight: "bold",
+            fontSize: 25,
+          },
+          headerStyle:{
+            height: 120,
+            backgroundColor: "yellow",            
+          }
+        }}
+        
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
